b561aaab6f0e3f39f40d7485533cf7a2c6218b64
Sanjnak <sanjna.amethi@gmail.com>
2025-05-20 15:19:56 +0530

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>CodeAlong website</h1>
-    </div>
+    <>
+      <BrowserRouter>
+       <Routes>
+        <Route path = "/" element = {<Home/>}></Route>
+       </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
