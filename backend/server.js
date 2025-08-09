c2ef47933922f08dd42687c9f34ebd891a634962
Sanjana Kumari <86369138+Sanjnak@users.noreply.github.com>
2025-08-09 15:29:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
