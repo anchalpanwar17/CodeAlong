69845624bef1c03679fbce03a922abc4dc6259ec
Soumya Upadhyay <99285192+SoumyaU25@users.noreply.github.com>
2025-08-10 15:07:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const path = require("path");
+const port = process.env.PORT || 5000
 
 
 const encode = (str) => Buffer.from(str).toString("base64");
@@ -394,6 +395,6 @@ app.get(/.*/, (req, res) => {
 });
 
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
